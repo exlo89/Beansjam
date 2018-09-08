fb9f7369fc7625dbd0965c0874ceeb39fa5108da
Exlo <m.appelmann@tu-bs.de>
2018-09-08 20:37:18 +0200

    generator finished

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -8,25 +8,28 @@ public class LevelGenerator : MonoBehaviour
     public int MaxGeneratedBlocks = 100;
     public float LevelSpeed = 2;
     [Range(1, 9)] public int SpawnLevelSize = 5;
-    [Range(-5, 5)] public int Offset;
+    [Range(-4, 4)] public int Offset;
+    [Range(0, 1)] public float[] Likelihood;
 
     public int BlockHeight = 10;
 
     private GameObject[] _currentBlocks;
     private int _borderBelow;
     private int _topOffset;
-    
+    private int _blockCounter;
+
     // Use this for initialization
     void Start()
     {
         _borderBelow = (int) Mathf.Round((SpawnLevelSize - Offset) * 0.5F) * BlockHeight;
         _topOffset = SpawnLevelSize % 2 == 0 ? 1 : 0;
 
-        Debug.Log(_borderBelow);
+        _blockCounter = SpawnLevelSize;
+
         _currentBlocks = new GameObject[SpawnLevelSize];
         for (int i = 0; i < SpawnLevelSize; i++)
         {
-            _currentBlocks[i] = Instantiate(LevelBlocks[i%LevelBlocks.Length],
+            _currentBlocks[i] = Instantiate(LevelBlocks[0],
                 transform.position + Vector3.down * _borderBelow +
                 Vector3.up * BlockHeight * i,
                 transform.rotation);
@@ -37,16 +40,32 @@ public class LevelGenerator : MonoBehaviour
     {
         float step = LevelSpeed * Time.deltaTime;
 
+        if (_blockCounter >= MaxGeneratedBlocks)
+        {
+            Debug.Log("finished game");
+        }
+
         for (int i = 0; i < _currentBlocks.Length; i++)
         {
+            if (_currentBlocks[i] == null)
+            {
+                continue;
+            }
+
             if (_currentBlocks[i].transform.position.y < -_borderBelow - BlockHeight)
             {
                 Destroy(_currentBlocks[i]);
-                int radommNumber = Random.Range(0, LevelBlocks.Length);
-                
-                _currentBlocks[i] = Instantiate(LevelBlocks[radommNumber],
-                    transform.position + Vector3.up * _borderBelow + Vector3.up * _topOffset * -BlockHeight,
-                    transform.rotation);
+                Debug.Log("=============================================");
+                int radommNumber = calculateRandomNumber();
+
+                _blockCounter++;
+
+                if (_blockCounter < MaxGeneratedBlocks)
+                {
+                    _currentBlocks[i] = Instantiate(LevelBlocks[radommNumber],
+                        transform.position + Vector3.up * _borderBelow + Vector3.up * _topOffset * -BlockHeight,
+                        transform.rotation);
+                }
             }
             else
             {
@@ -54,5 +73,19 @@ public class LevelGenerator : MonoBehaviour
                     _currentBlocks[i].transform.position + Vector3.down, step);
             }
         }
+    }
+
+    private int calculateRandomNumber()
+    {
+        float tempRdm = Random.value; //0.1
+        for (int i = 0; i < LevelBlocks.Length; i++)
+        {
+            if (tempRdm >= Likelihood[i])
+            {
+                return i;
+            }
+        }
+
+        return LevelBlocks.Length-1;
     }
 }
